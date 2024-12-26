57e6eec30c5f4b345e38d01174323a6bb4660c63
kdongjin <rlaeogus0911@nate.com>
2024-12-27 08:57:15 +0900

    리액트 테스팅

---
+++ section03/src/index1.js
@@ -0,0 +1,6 @@
+//외부에 있는 모듈을 가져온다.(import)
+ //const moduleData =  require('./math');
+ //const {add,sub} =  require('./math');
+ import {add,sub} from './math.js';
+ console.log(add(30,20)); 
+ console.log(sub(30,20));
